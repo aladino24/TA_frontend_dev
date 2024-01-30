f0717f9e41a7662ed43eef1288864815b13d2bf0
komodo22 <aladinozulmar@gmail.com>
2024-01-30 15:03:35 +0700

    30/01/2024 ---> Master Brand Edit

--- src/pages/contents/datamaster/master-brand/DashboardMasterBrand.js
+++ src/pages/contents/datamaster/master-brand/DashboardMasterBrand.js
@@ -1,11 +1,26 @@
 import axios from "axios";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Config from "../../../../config";
 import $ from "jquery";
 import ModalBrand from "./ModalBrand";
+import SweetAlertLoading from "../../../../components/SweetAlertLoading";
+import SweetAlertError from "../../../../components/SweetAlertError";
+import SweetAlertSuccess from "../../../../components/SweetAlertSuccess";
 
 const DashboardMasterBrand = () => {
     const tableRef = useRef(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [success, setSuccess] = useState(false);
+    const [message, setMessage] = useState('');
+    const [inputData, setInputData] = useState({
+        id: "",
+        fc_divisioncode: "",
+        fc_branch: "",
+        fc_brand: "",
+        fc_group: "",
+        fc_subgroup: ""
+    });
     
     useEffect(() => {
         const fetchData = async () => {
@@ -60,11 +75,19 @@ const DashboardMasterBrand = () => {
                     ],
                     rowCallback: function(row, data) {
                         const actionBtns = `
-                        <button class="btn btn-sm btn-primary" id="editBtn">Edit</button>
+                        <button class="btn btn-sm btn-warning" id="editBtn">Edit</button>
+                        <button class="btn btn-sm btn-danger" id="deleteBtn">Delete</button>
                         `;
                         $("td:eq(6)", row).html(actionBtns);
                     }
                 });
+
+                $(tableRef.current).on("click", "#editBtn", function(e) {
+                    const rowData = table.row($(this).parents("tr")).data();
+                    detailEditBrand(rowData);
+                    window.$('#editModalBrand').modal('show');
+                });
+
 
                 if(response.data.success){
                     // console.log(responseData);
@@ -84,6 +107,85 @@ const DashboardMasterBrand = () => {
             existingTable.destroy();
           };
     },[]);
+
+    const detailEditBrand = async(data) => {
+        // const token = localStorage.getItem('token');
+
+        if(data){
+            setInputData({
+                id: data.id ? data.id : "",
+                fc_divisioncode: data.fc_divisioncode ? data.fc_divisioncode : "",
+                fc_branch: data.fc_branch ? data.fc_branch : "",
+                fc_brand: data.fc_brand ? data.fc_brand : "",
+                fc_group: data.fc_group ? data.fc_group : "",
+                fc_subgroup: data.fc_subgroup ? data.fc_subgroup : ""
+            });
+        }
+    }
+
+    const handleEditBrand = async(e) => {
+        e.preventDefault();
+        const token = localStorage.getItem('token');
+
+        setLoading(true);
+        const data = {
+            id: inputData.id,
+            fc_divisioncode: inputData.fc_divisioncode,
+            fc_branch: inputData.fc_branch,
+            fc_brand: inputData.fc_brand,
+            fc_group: inputData.fc_group,
+            fc_subgroup: inputData.fc_subgroup
+        }
+        const apiurl = Config.api.server2 + `master/brand`;
+
+        try {
+            const response = await axios({
+                method: "put",
+                url: apiurl,
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+                data: data
+            });
+
+            if(response.data.success){
+                setSuccess(true);
+                setLoading(false);
+                setMessage(response.data.message);
+            }else{
+                setError(true);
+                setLoading(false);
+            }
+        } catch (error) {
+            setError(true);
+            setLoading(false);
+            if (error.response) {
+                console.log('Response Data:', error.response.data);
+                console.log('Response Status:', error.response.status);
+                console.log('Response Headers:', error.response.headers);
+            } else if (error.request) {
+                console.log('Request made but no response received:', error.request);
+            } else {
+                console.log('Error during request setup:', error.message);
+            }
+            console.log('Config:', error.config);
+        } finally {
+            setLoading(false);
+        }
+
+    }
+
+    const handleCloseErrorModal = () => {
+        setError(null);
+      };
+    
+
+    const handleSuccessAlertClose = () => {
+        setSuccess(false);
+        // Reload the page upon successful API response
+        window.location.reload();
+      };
+
     return (
         <>
             <div className="container-fluid">
@@ -134,7 +236,133 @@ const DashboardMasterBrand = () => {
                     </div>
                 </div>
             </div>
+
+            <div
+            className="modal fade"
+            id="editModalBrand"
+            tabIndex="-1"
+            role="dialog"
+            aria-labelledby="editModalBrandLabel">
+            <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                 <div className="modal-header">
+                        <h5 className="modal-title" id="editModalLabel">
+                            Edit Brand
+                        </h5>
+                        <button
+                            type="button"
+                            className="close"
+                            data-dismiss="modal"
+                            aria-label="Close"
+                        >
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                  </div>
+
+                <form onSubmit={handleEditBrand}>
+                  <div className="modal-body">
+                        <div className="form-group">
+                            <label htmlFor="fc_branch">Cabang</label>
+                            <input 
+                                type="text" 
+                                className="form-control" 
+                                id="fc_divisioncode" 
+                                value={inputData.fc_divisioncode}  
+                                onChange={
+                                    (e) => setInputData(prevInputData => ({
+                                        ...prevInputData,
+                                        fc_divisioncode: e.target.value,
+                                    }))
+                                }
+                                hidden 
+                            />
+                            <input 
+                                type="text" 
+                                className="form-control" 
+                                id="fc_branch" 
+                                value={inputData.fc_branch} 
+                                onChange={
+                                    (e) => setInputData(prevInputData => ({
+                                        ...prevInputData,
+                                        fc_branch: e.target.value,
+                                    }))
+                                }
+                                readOnly
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="fc_brand">Brand</label>
+                            <input 
+                                type="text" 
+                                className="form-control" 
+                                id="fc_brand"
+                                name="fc_brand"  
+                                value={inputData.fc_brand}
+                                onChange={
+                                    (e) => setInputData(prevInputData => ({
+                                        ...prevInputData,
+                                        fc_brand: e.target.value,
+                                    }))
+                                }
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="fc_group">Group</label>
+                            <input 
+                                type="text" 
+                                className="form-control" 
+                                id="fc_group" 
+                                name="fc_group" 
+                                value={inputData.fc_group}
+                                onChange={
+                                    e => setInputData(prevInputData => ({
+                                        ...prevInputData,
+                                        fc_group: e.target.value,
+                                    }))
+                                }
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="fc_subgroup">Sub Group</label>
+                            <input 
+                                type="text" 
+                                className="form-control" 
+                                id="fc_subgroup"  
+                                name="fc_subgroup"
+                                value={inputData.fc_subgroup}
+                                onChange={
+                                    e => setInputData(prevInputData => ({
+                                        ...prevInputData,
+                                        fc_subgroup: e.target.value,
+                                    }))
+                                }
+                            />
+                        </div>
+
+                  </div> 
+                    <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal">
+                                Tutup
+                            </button>
+                            <button type="submit" className="btn btn-primary" id="saveChangesBtn">
+                                Simpan
+                            </button>
+                        </div>
+                        </form>
+                </div>
+                
+            </div>
+            </div>
             <ModalBrand id="addModalBrand" />
+
+            <SweetAlertLoading show={loading} />
+            <SweetAlertError show={!!error} message={error} onClose={handleCloseErrorModal} />
+            <SweetAlertSuccess 
+                show={success}
+                message={message}
+                onClose={handleSuccessAlertClose}
+            
+            />
         </>
     );
 }
