dbfd65aad7917c0cc087385e617752c4b01e7673
komodo22 <aladinozulmar@gmail.com>
2024-04-09 14:32:08 +0700

    feat: Get Stock Barang From Distributor

--- src/pages/contents/request/create/ModalSelectBarang.js
+++ src/pages/contents/request/create/ModalSelectBarang.js
@@ -1,7 +1,89 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import $, { data } from "jquery";
+import axios from "axios";
+import Config from "../../../../config";
 
 const ModalSelectBarang = (props) => {
-    const { id, selectedRowData } = props;
+  const tableRef = useRef(null);
+  const { id, selectedRowData } = props;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const fc_distributorcode = selectedRowData.fc_distributorcode
+          ? btoa(selectedRowData.fc_distributorcode)
+          : "";
+        const token = localStorage.getItem("token");
+        const axiosConfig = {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        };
+
+        const response = await axios.get(
+          Config.api.server3 + "stock-barang/" + fc_distributorcode,
+          axiosConfig
+        );
+
+        const responseData = response.data.data;
+        if ($.fn.DataTable.isDataTable("#dataTable")) {
+          // If it is initialized, destroy it before reinitializing
+          const existingTable = $(tableRef.current).DataTable();
+          existingTable.destroy();
+        }
+
+        const formattedData = responseData
+          ? responseData.map((distributor, index) => ({
+              ...distributor,
+              no: index + 1,
+            }))
+          : [];
+
+        const table = $(tableRef.current).DataTable({
+          responsive: true,
+          processing: true,
+          serverSide: false, // Ubah menjadi true jika ingin implementasi server-side processing
+          data: formattedData,
+          columnDefs: [],
+          columns: [
+            { data: "no" },
+            { data: "stock.fc_stockcode" },
+            { data: "stock.fc_namelong" },
+            { data: "stock.fc_nameshort" },
+            { data: "stock.fc_brand" },
+            { data: "stock.fc_subgroup" },
+            { data: "stock.fc_typestock1" },
+            { data: "fc_batch" },
+            {
+              data: null,
+            },
+          ],
+          rowCallback: function (row, data) {
+            const actionBtns = `
+                  <button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#selectBarang" id="selectButton">
+                      <i class="fas fa-shopping-cart"></i>
+                  </button>
+                  `;
+            $("td:eq(8)", row).html(actionBtns);
+
+            $(tableRef.current).on("click", "#selectButton", function () {
+              //modal show
+              window.$("#selectBarang").modal("show");
+            });
+          },
+        });
+      } catch (error) {}
+    };
+
+    fetchData();
+
+    return () => {
+      // Hancurkan DataTable saat komponen dilepas
+      const existingTable = $(tableRef.current).DataTable();
+      existingTable.destroy();
+    };
+  }, [selectedRowData]);
   return (
     <div
       className="modal fade"
@@ -10,10 +92,16 @@ const ModalSelectBarang = (props) => {
       role="dialog"
       aria-labelledby="editModalLabel"
     >
-      <div className="modal-dialog modal-xl" role="document">
+      <div
+        className="modal-dialog modal-xl"
+        role="document"
+        style={{ maxWidth: "90%", maxHeight: "90%" }}
+      >
         <div className="modal-content">
           <div className="modal-header">
-            <h5 className="modal-title">{selectedRowData ? selectedRowData.fc_distributorname1 : ''}</h5>
+            <h5 className="modal-title">
+              {selectedRowData ? selectedRowData.fc_distributorname1 : ""}
+            </h5>
             <button
               type="button"
               className="close"
@@ -23,7 +111,46 @@ const ModalSelectBarang = (props) => {
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
-          <div className="modal-body">{props.children}</div>
+          <div className="modal-body">
+            <div className="card-body">
+              <div className="table-responsive">
+                <table
+                  className="table table-bordered mt-2"
+                  ref={tableRef}
+                  id="dataTable"
+                  width="100%"
+                  cellSpacing="0"
+                >
+                  <thead className="bg-primary text-light">
+                    <tr>
+                      <th>No</th>
+                      <th>Kode Barang</th>
+                      <th>Nama Barang</th>
+                      <th>Sebutan</th>
+                      <th>Brand</th>
+                      <th>Sub Group</th>
+                      <th>Tipe Stock</th>
+                      <th>Batch</th>
+                      <th>Actions</th>
+                    </tr>
+                  </thead>
+                  <tfoot className="bg-primary text-light">
+                    <tr>
+                      <th>No</th>
+                      <th>Kode Barang</th>
+                      <th>Nama Barang</th>
+                      <th>Sebutan</th>
+                      <th>Brand</th>
+                      <th>Sub Group</th>
+                      <th>Tipe Stock</th>
+                      <th>Batch</th>
+                      <th>Actions</th>
+                    </tr>
+                  </tfoot>
+                </table>
+              </div>
+            </div>
+          </div>
           <div className="modal-footer">
             <button
               type="button"
